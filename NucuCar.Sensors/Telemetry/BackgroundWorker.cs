572c12d43f5829f7dda27ce3d211a98114ef8080
Denis-Cosmin Nutiu <dnutiu@bitdefender.com>
2019-11-14 19:23:16 +0200

    Attempt to add Google Cloud Platform as a MQTT server

--- NucuCar.Sensors/Telemetry/BackgroundWorker.cs
+++ NucuCar.Sensors/Telemetry/BackgroundWorker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -9,6 +8,11 @@ namespace NucuCar.Sensors.Telemetry
 {
     public class BackgroundWorker : BackgroundService
     {
+        private readonly string _projectId;
+        private readonly string _region;
+        private readonly string _registryId;
+        private readonly string _deviceId;
+        private readonly string _rs256KeyFile;
         private readonly bool _serviceEnabled;
         private readonly int _interval;
         private readonly ILogger _logger;
@@ -19,6 +23,11 @@ namespace NucuCar.Sensors.Telemetry
             var configSection = configuration.GetSection("Telemetry");
             _serviceEnabled = configSection.GetValue<bool>("Enabled");
             _interval = configSection.GetValue<int>("Interval");
+            _projectId = configSection.GetValue<string>("ProjectId");
+            _region = configSection.GetValue<string>("Region");
+            _registryId = configSection.GetValue<string>("RegistryId");
+            _deviceId = configSection.GetValue<string>("DeviceId");
+            _rs256KeyFile = configSection.GetValue<string>("RS256File");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -27,13 +36,23 @@ namespace NucuCar.Sensors.Telemetry
             {
                 return;
             }
+
             await Task.Delay(_interval, stoppingToken);
 
             using var telemetryService = TelemetryService.Instance;
+
+            telemetryService.SetLogger(_logger);
+            telemetryService.ProjectId = _projectId;
+            telemetryService.DeviceId = _deviceId;
+            telemetryService.RegistryId = _registryId;
+            telemetryService.Region = _region;
+            telemetryService.Rs256File = _rs256KeyFile;
+
+            await telemetryService.StartAsync();
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Publishing telemetry data!");
-                await telemetryService.PublishData();
+                await telemetryService.PublishDataAsync(stoppingToken);
                 await Task.Delay(_interval, stoppingToken);
             }
         }
