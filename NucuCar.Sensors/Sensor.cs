0a5319d723f0644c3c94cfd81f2fda1b725c00e1
Denis-Cosmin Nutiu <dnutiu@bitdefender.com>
2019-11-30 18:58:27 +0200

    Remove Sensor.cs

--- NucuCar.Sensors/Sensor.cs
+++
@@ -1,9 +0,0 @@
-using NucuCar.Domain.Telemetry;
-
-namespace NucuCar.Sensors
-{
-    public class Sensor<T> : ISensor<T> where T : class, ITelemeter
-    {
-        public T Object { get; }
-    }
-}
