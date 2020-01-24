805a132183c0848baecb02a2cbb7948a749a3d15
Denis-Cosmin Nutiu <dnutiu@bitdefender.com>
2020-01-24 15:13:09 +0200

    Fix bug in TelemetryPublisherDisk not flushing & add file extension in connection string

--- NucuCar.Domain/Telemetry/TelemetryPublisherDisk.cs
+++ NucuCar.Domain/Telemetry/TelemetryPublisherDisk.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -15,12 +16,14 @@ namespace NucuCar.Domain.Telemetry
     public class TelemetryPublisherDisk : TelemetryPublisher
     {
         private readonly FileStream _fileStream;
+        private int _charactersWritten;
 
         /// <summary>
         /// Constructs an instance of <see cref="TelemetryPublisherDisk"/>.
         /// <remarks>
         ///    The connection string must contain the following options:
         ///     Filename (required) - The path of the filename in which to log telemetry data.
+        ///     FileExtension (optional) - The extension of the filename. Default txt
         ///     BufferSize (optional) - The buffer size for the async writer. Default: 4096
         /// </remarks>
         /// </summary>
@@ -28,10 +31,11 @@ namespace NucuCar.Domain.Telemetry
         public TelemetryPublisherDisk(TelemetryPublisherBuilderOptions opts) : base(opts)
         {
             var connectionStringParams = ConnectionStringParser.Parse(opts.ConnectionString);
-            var fileName = connectionStringParams.GetValueOrDefault("Filename");
+            var fileName = connectionStringParams.GetValueOrDefault("FileName");
+            var fileExtension = connectionStringParams.GetValueOrDefault("FileExtension", "txt");
             var bufferSize = connectionStringParams.GetValueOrDefault("BufferSize", "4096");
 
-            _fileStream = new FileStream(fileName, FileMode.Append, FileAccess.Write,
+            _fileStream = new FileStream(NormalizeFilename(fileName, fileExtension), FileMode.Append, FileAccess.Write,
                 FileShare.Read, int.Parse(bufferSize), true);
             Logger?.LogDebug("Initialized the TelemetryPublisherDisk!");
         }
@@ -43,13 +47,21 @@ namespace NucuCar.Domain.Telemetry
             Logger?.LogDebug($"Telemetry message: {messageString}");
             var encodedText = Encoding.Unicode.GetBytes(messageString);
 
-            await _fileStream.WriteAsync(encodedText, (int) _fileStream.Length,
+            await _fileStream.WriteAsync(encodedText, _charactersWritten,
                 encodedText.Length, cancellationToken);
+            _charactersWritten += encodedText.Length;
+            await _fileStream.FlushAsync(cancellationToken);
         }
 
         public override void Dispose()
         {
             _fileStream.Close();
+        }
+
+        private static string NormalizeFilename(string filename, string extension)
+        {
+            var date = DateTime.UtcNow;
+            return $"{filename}-{date.Year}-{date.Month}-{date.Day}_{date.ToFileTimeUtc()}.{extension}";
         }
     }
 }
