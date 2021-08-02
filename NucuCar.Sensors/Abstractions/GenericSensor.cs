f758501c037cc178832835e4aacc26d1d0c98bd3
Denis Nutiu <denis.nutiu@gmail.com>
2021-08-02 22:00:36 +0300

    Add TODO in GenericSensor.cs.

--- NucuCar.Sensors/Abstractions/GenericSensor.cs
+++ NucuCar.Sensors/Abstractions/GenericSensor.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 
 namespace NucuCar.Sensors.Abstractions
 {
+    // TODO: Revisit abstractions and check for bad design.
     /// <summary>
     /// The GenericSensor is an abstract class, which provides a base for abstracting hardware sensors.
     /// </summary>
